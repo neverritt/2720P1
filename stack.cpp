516e7b79381903eb710eb9d4a767869f0cb7568e
PATRICK STROUD <pstroud@nike.cs.uga.edu>
2015-09-26 17:48:27 -0400

    Almost finished LinkedStack

--- stack.cpp
+++ stack.cpp
@@ -8,9 +8,10 @@ using std::endl;
 using std::string;
 int main(int argc, const char * argv[]) {
 
-cout << "Testing Stack Implementations..." << endl;
+
+// ArrayStack Testing
+cout << "Testing Array Stack Implementations..." << endl;
 Stack<int> * s1 = new ArrayStack<int> {};
-// Stack<int> * s2 = new LinkedStack<int> {};
 // test!
 s1->push(1);
 s1->push(2);
@@ -31,11 +32,34 @@ cout << "Peeking " << s1->peek() << endl;
 cout << "size is " << s1->size() << endl;
 delete s1;
 
-// delete s2;
+cout << endl << endl;
+
+
+// LinkedStack Testing
+cout << "Testing LinkedStack . . . " << endl;
+Stack<int> * s2 = new LinkedStack<int> {};
+s2->push(5);
+s2->push(4);
+s2->push(3);
+s2->push(2);
+s2->push(1);
+cout << "Peek: " << s2->peek() << endl;
+cout << "Popping: " << s2->pop() << endl;
+cout << "Peek: " << s2->peek() << endl;
+cout << "Size: " << s2->size() << endl;
+cout << "Is empty? " << s2->empty() << endl;
+cout << "Test copy constructor: " << endl;
+Stack<int> * s3 = s2;
+cout << "Top of new stack: " << s3->peek() << endl;
+delete s2;
+delete s3;
+
+cout << endl << endl;
+
 
 // testing linked list implementation
 
-LinkedList<int> list;
+LinkedList<int> list = new LinkedList<int> ();
 cout << "initial size is: " << list.size() << endl;
 
 list.prepend( 1 );
